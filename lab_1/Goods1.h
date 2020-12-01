d1680d57085930253cafbdf2cb6cc602a05d5c2c
DavrosWho <71879137+DavrosWho@users.noreply.github.com>
2020-12-01 20:23:19 +0600

    Add files via upload

--- lab_1/Goods1.h
+++ lab_1/Goods1.h
@@ -1,26 +1,50 @@
 #pragma once
+#include <time.h>
+#include <iostream>
+#include <fstream>
 
 class Goods
 {
 	public:
 		Goods();
-		Goods(char* aName, int aWCost, int aRCost, int aGoodsQuantity, char* aDate);
+		Goods(char* aName, int aGoodsQuantity, int aWCost, int aRCost, time_t* aDate);
+		Goods& operator=(Goods aGoods);
+		Goods& operator=(const char* strGoods);
+		Goods operator++(int);
+		Goods operator--(int);
+		Goods operator++();
+		Goods operator--();
+		friend Goods operator+(int aQuantity, Goods aGoods);
+		friend Goods operator+(Goods aGoods, int aQuantity);
+		friend Goods operator-(Goods aGoods, int aQuantity);
+		operator double();
+		friend std::ofstream& operator << (std::ofstream& fout, Goods aGoods);
+		friend std::ifstream& operator >> (std::ifstream& fin, Goods& aGoods);
+		friend std::ostream& operator << (std::ostream& out, Goods aGoods);
+		friend std::istream& operator >> (std::istream& in, Goods& aGoods);
+		void write(std::ofstream& fout);
+		void read(std::ifstream& fin);
 		static int goodsCount;
 		void changeQuantity(int aGoodsQuantity);
 		void changeName(char* aName);
-		void changeDate(char* aDate);
+		void changeDate(time_t* aDate);
 		void toString(char* strGoods);
 		void changeWCost(int aWCost);
 		void markup(int aMarkup);
 		void markdown(int aMarkdown);
+		char* getName() { return goodsName; };
 		int getMarkup() { return retailCost - wholesaleCost; };
 		int getWCost() { return wholesaleCost; };
 		int getRCost() { return retailCost; };
+		time_t* getDate() { return goodsDate; };
+		int getQuantity() { return goodsQuantity; };
+		char* getStr() { return str; };
 	private:
+		char* str = new char[97];
 		char* goodsName;
 		int goodsQuantity;
 		int wholesaleCost;
 		int retailCost;
-		char* goodsDate;
+		time_t* goodsDate;
 };
 
--- lab_1/Laba-3-1.cpp
+++ lab_1/Laba-3-1.cpp
@@ -1,10 +1,41 @@
 ﻿#include <iostream>
+#include <fstream>
 #include "Goods1.h"
 using namespace std;
 
-//Тест на возможность смены наименования товара
-bool testName(int nameLength) {
-	if (nameLength > 15 || nameLength < 0) {
+bool testFileTXT(Goods aGoods, Goods bGoods) {
+	cout << "Text file:" << endl;
+	cout << "Added to file" << endl;
+	ofstream fout("data.txt", ios::app);
+	if (!fout) {
+		return false;
+	}
+	else {
+		fout << aGoods;
+		cout << aGoods << endl;
+	}
+	fout.close();
+	cout << endl;
+
+	cout << "Loaded from file" << endl;
+	ifstream fin("data.txt");
+	if (!fin) {
+		return false;
+	}
+	else {
+		fin >> bGoods;
+		cout << bGoods << endl;
+	}
+	fin.close();
+
+	bool error = false;
+	if (aGoods.getQuantity() != bGoods.getQuantity()) {
+		error = true;
+	}
+	
+	cout << endl;
+
+	if (error) {
 		return false;
 	}
 	else {
@@ -12,39 +43,40 @@ bool testName(int nameLength) {
 	}
 }
 
-//Тест на возможность смены количества единиц товара
-bool testQuantity(int aQuantity) {
-	if (aQuantity < 0 || aQuantity > 9999999) {
+bool testFileDAT(Goods aGoods, Goods bGoods) {
+	cout << "Binary file:" << endl;
+	cout << "Added to file" << endl;
+
+	ofstream fout("data.dat", ios::binary);
+	if (!fout) {
 		return false;
 	}
 	else {
-		return true;
+		aGoods.write(fout);
+		cout << aGoods << endl;
 	}
-}
+	fout.close();
+	cout << endl;
 
-//Тест на возможность смены оптовой стоимости товара
-bool testWCost(Goods aGoods, int aWCost) {
-	if (aGoods.getRCost() < aWCost) {
+	cout << "Loaded from file" << endl;
+	ifstream fin("data.dat");
+	if (!fin) {
 		return false;
 	}
 	else {
-		return true;
+		bGoods.read(fin);
+		cout << bGoods << endl;
 	}
-}
+	fin.close();
 
-//Тест на возможность смены наценки товара
-bool testMarkup(Goods aGoods, int aMarkup) {
-	if (aGoods.getWCost() + aMarkup > 9999999 || aMarkup < 0) {
-		return false;
+	bool error = false;
+	if (aGoods.getQuantity() != bGoods.getQuantity()) {
+		bool error = true;
 	}
-	else {
-		return true;
-	}
-}
 
-//Тест на возможность смены уценки товара
-bool testMarkdown(Goods aGoods, int aMarkdown) {
-	if (aGoods.getMarkup() < 0 || aMarkdown < 0) {
+	cout << endl;
+
+	if (error == true) {
 		return false;
 	}
 	else {
@@ -54,109 +86,42 @@ bool testMarkdown(Goods aGoods, int aMarkdown) {
 
 int main()
 {
+
+	ofstream fout("data.txt");
+	fout << "";
+	fout.close();
+	ofstream fout2("data.dat");
+	fout2 << "";
+	fout2.close();
+	char* strGoods = new char[100];
 	Goods Goods1;
-	char* strGoods1 = new char[100];
-	const int nameLength1 = 25;
-	char name1[nameLength1] = "Raxacoricofallapatorians";
-	cout << name1 << " goods ";
-	if (testName(nameLength1)) {
-		cout << "can be registered.\nChange-name-test was successfully\nOur goods: ";
-		Goods1.changeName(name1);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
-	}
-	else {
-		cout << "can't be registered. There cannot be more than 15 symbols\nCange-name-test failed\n\n";
-	}
+	Goods Goods2;
+	Goods Goods3;
+
+	cout << "Enter goods: ";
+	cin >> Goods1;
+	cout << "First goods: " << Goods1 << '\n';
+	cout << "Second goods: " << Goods2 << "\n\n";
 	
-	const int nameLength2 = 15;
-	char name2[nameLength2] = "Milk";
-	cout << name2 << " goods ";
-	if (testName(nameLength2)) {
-		cout << "can be registered.\nChange-name-test was successfully\nOur goods: ";
-		Goods1.changeName(name2);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
+	if (testFileTXT(Goods1, Goods2)) {
+		cout << "Adding and loading objects from a text file test completed successfully" << endl;
 	}
 	else {
-		cout << "can't be registered. There cannot be more than 15 symbols\nCange-name-test failed\n\n";
+		cout << "Adding and loading objects from a text file test completed failed" << endl;
 	}
 
-	int quantity1 = -100;
-	if (testQuantity(quantity1)) {
-		cout << "We can store " << quantity1 << " units of goods.\nChange-quantity-test was successfully\nOur goods:";
-		Goods1.changeQuantity(quantity1);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
+	cout << "\n\n";
+
+	if (testFileDAT(Goods1, Goods2)) {
+		cout << "Adding and loading objects from a text file test completed successfully" << '\n';
 	}
 	else {
-		cout << "We can't store" << quantity1 << " units of goods. Our warehouse accommodates from 0 to 9999999 units of one goods\nChange-quantity-test failed\n\n";
+		cout << "Adding and loading objects from a text file test completed failed" << '\n';
 	}
 
-	quantity1 = 215;
-	if (testQuantity(quantity1)) {
-		cout << "We can store " << quantity1 << " units of goods.\nChange-quantity-test was successfully\nOur goods:";
-		Goods1.changeQuantity(quantity1);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
-	}
-	else {
-		cout << "We can't store" << quantity1 << " units of goods. Our warehouse accommodates from 0 to 9999999 units of one goods\nChange-quantity-test failed\n\n";
-	}
+	cout << '\n';
 
-	int wCost1 = 200;
-	if (testWCost(Goods1, wCost1)) {
-		cout << "Now wholesale cost is " << wCost1 << ".\nChange-WCost-test was successfully\nOur goods:";
-		Goods1.changeWCost(wCost1);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
-	}
-	else {
-		cout << "Wholesale cost can't be " << wCost1 << ". It's bigger than retail cost\nChange-WCost-test failed\n\n";
-	}
-
-	int markup1 = 101;
-	if (testMarkup(Goods1, markup1)) {
-		cout << "Change the value of the markup by " << markup1 << " coins.\nChange-Markup-test was successfulled\nOur goods:";
-		Goods1.markup(markup1);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
-	}
-	else {
-		cout << "Markup can't be " << wCost1 << "coins. It's always > 0\nChange-Markup-test failed\n\n";
-	}
-
-	int markdown1 = 32;
-	if (testMarkdown(Goods1, markdown1)) {
-		cout << "We can add markdown of " << markdown1 << " coins.\nChange-Markdown-test was successfulled\nOur goods:";
-		Goods1.markdown(markdown1);
-		Goods1.toString(strGoods1);
-		for (int i = 0; i < 100; i++) {
-			cout << strGoods1[i];
-		}
-		cout << "\n\n";
-	}
-	else {
-		cout << "We can't add markdown of " << markdown1 << " coins. Wholesale cost can't be less than retail cost.\nChange-Markdown-test failed\n\n";
-	}
+	cout << "Text-output-test. Third goods: " << Goods3 << '\n'; 
 
 	cout << Goods::goodsCount << " unique goods are stored in the warehouse.";
 }
