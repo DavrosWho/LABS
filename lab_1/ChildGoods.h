04c1ffdf8d88bbfd606d5723234270d34dcac50a
DavrosWho <71879137+DavrosWho@users.noreply.github.com>
2021-01-09 13:33:00 +0600

    Update 1.1

--- lab_1/ChildGoods.h
+++ lab_1/ChildGoods.h
@@ -6,7 +6,7 @@ class ChildGoods :
 public:
 	ChildGoods();
 	ChildGoods(char* aName, int aGoodsQuantity, int aWCost, int aRCost, time_t* aDate, int aCategory, int aType);
-	char* toString();
+	char* toString() override;
 	int getCategory() { return category; };
 	friend std::ostream& operator << (std::ostream& out, ChildGoods& aGoods);
 private:
